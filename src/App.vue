5b38a03e27547eb1f2c77af0d8b9f81ca861881a
KleinzDev <kleinzmail@gmail.com>
2022-05-16 21:29:50 +0800

    delete my website out of existence

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script setup>
-  import Navbar from './components/Navbar.vue';
-</script>
-
 <template>
-  <div class="flex flex-col h-screen">
-    <Navbar />
-    <router-view></router-view>
-  </div>
+  ye (coming soon fr)
 </template>
--- src/views/PageNotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="flex flex-grow">
-        <div class="m-auto">
-            <h1 class="text-3xl font-light">404 | Not Found</h1>
-        </div>
-    </div>
-</template>
